07bf670d53a5aa6b02c939d8b5f7ed37c0620d6a
Konstantin Balandin <kostabalandin169@gmail.com>
2026-08-26 21:19:24 +0700

    include routers

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
+from src.api.router import router as collections_router
+from src.api.cache_router import router as cache_router
 
 
 app = FastAPI(title="FAISS rag service")
+
+
+app.include_router(collections_router)
+app.include_router(cache_router)
